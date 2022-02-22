a62d05ba9d3827fd77e92e85d70c3a14ed649edf
Yalan Huang <ya.huang2@live.edu.au>
2022-02-22 21:58:31 +1100

    new file

---
+++ workshop1.R
@@ -0,0 +1,23 @@
+150/3
+a <- 10
+a
+b <- "cool"
+b
+b
+b
+c <- c(3,4,7,8)
+c
+c
+summary(c)
+
+
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("tidyverse")
+library(tidyverse)
+rm(b)
+
+head(mtcars)
+data(mtcars)
+summary(mtcars)
+glimpse(mtcars)
